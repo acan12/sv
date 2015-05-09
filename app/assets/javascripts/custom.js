c960d1da0160963c514ff1797a811d4692c7a4c8
arycancer <arycancer@gmail.com>
2015-05-10 01:07:12 +0700

    add mosaic style for main content

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,32 @@
 /* Your JS codes here */
+$(document).ready(function() {
+
+  $("#mosaic").nested({
+      selector: '.box',
+      gutter: 6,
+      animate: false
+
+  });
+  
+  //   var wall = new freewall("#mosaic");
+  //   wall.reset({
+  //  selector: '.item',
+  //  animate: true,
+  //  cellW: 150,
+  //  cellH: 'auto',
+  //  onResize: function() {
+  //    wall.fitWidth();
+  //  }
+  // });
+  // 
+  // ab = function(){
+  //   var boxes = makeBoxes();
+  //   $("#mosaic").prepend(boxes).nested('prepend', boxes);
+  // }
+  // 
+  // 
+  // setInterval(ab, 5000);
+  
+
+  
+});
