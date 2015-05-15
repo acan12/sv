e1c9ff638cba78a02b9682fde6d57288cce8582e
arycancer <arycancer@gmail.com>
2015-05-15 14:53:04 +0700

    fix design improving for header + index + detail

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
 
   });
   
+  $(".hgx").hover(
+    function(){
+      
+      var currElementTop    = $(this).position().top;
+      var currScrollTop     = $(document).scrollTop();
+      
+      var offset = 30;
+      var size = (currScrollTop > currElementTop ) ? (currScrollTop - currElementTop + offset) : 0
+      
+      $(this).css({'top': size})
+      
+    }, function(){
+      
+      console.log("normal coordinate!!");
+      $(this).css({'top': 0})
+    }
+  );
+  
+  $(".hg").mosaic({
+    animation : 'slide'
+  });
+  
   //   var wall = new freewall("#mosaic");
   //   wall.reset({
   //  selector: '.item',
